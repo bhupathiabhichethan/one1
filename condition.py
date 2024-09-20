87d004d94b0568ecfa1f547ee46fe2b5628c6704
bhupathiabhichethan <168263310+bhupathiabhichethan@users.noreply.github.com>
2024-09-20 10:30:01 +0530

    Add files via upload
    good program

---
+++ condition.py
@@ -0,0 +1,9 @@
+post=input("entet the post")
+if("Abhi" in post):
+    print("abhi is good")
+else:
+    print("its ok ")   
+if("Abhi".lower() in post.lower()):#converts anycase to lower in post
+    print("abhi is good")
+else:
+    print("its ok ")   
