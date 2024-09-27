f24b72c53416b695f47422181c00599942d02e38
bhupathiabhichethan <168263310+bhupathiabhichethan@users.noreply.github.com>
2024-09-27 10:19:45 +0530

    Delete condition.py

--- condition.py
+++
@@ -1,9 +0,0 @@
-post=input("entet the post")
-if("Abhi" in post):
-    print("abhi is good")
-else:
-    print("its ok ")   
-if("Abhi".lower() in post.lower()):#converts anycase to lower in post
-    print("abhi is good")
-else:
-    print("its ok ")   
